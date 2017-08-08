762886ade5d25d0627c469e7fcafa0561fcdb1b1
lishihua <lishihua@100tal.com>
2017-08-08 09:57:46 +0800

    add group

---
+++ MyObject/MyObject/LSHObject.h
@@ -0,0 +1,13 @@
+//
+//  LSHObject.h
+//  MyObject
+//
+//  Created by 李世华 on 2017/8/8.
+//  Copyright © 2017年 zhikang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LSHObject : NSObject
+
+@end
---
+++ MyObject/MyObject/LSHObject.m
@@ -0,0 +1,13 @@
+//
+//  LSHObject.m
+//  MyObject
+//
+//  Created by 李世华 on 2017/8/8.
+//  Copyright © 2017年 zhikang. All rights reserved.
+//
+
+#import "LSHObject.h"
+
+@implementation LSHObject
+
+@end
